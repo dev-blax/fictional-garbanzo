ac29adca1aefee0074ca9d9eb8581beb85886e65
dev-blax <dev.blax@gmail.com>
2024-05-03 00:08:59 +0300

    configured material tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { LoomCard } from "./components/LoomCard";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <>
+    <LoomCard />
+   </>
   )
 }
